85572193993c939d3449bbef236d4d52dc3ed075
Andrii <mecushka@gmail.com>
2023-03-10 12:56:07 +0200

    test

---
+++ src/js/03-feedback.js
@@ -0,0 +1,21 @@
+import '../css/03-feedback.css';
+import '../css/common.css';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('email', onEmail);
+refs.textarea.addEventListener('textarea', onTextarea);
+
+function onTextarea(e) {
+    const message = e.currentTarget.value;
+
+    localStorage.setItem('feedback-msg', message)
+}
+
+const meaasge = "hallo";
+console.log(message);
