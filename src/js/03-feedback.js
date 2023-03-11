33dc9a3ab68ed4ba5ac44b2cb4d2f5b202d95af6
Andrii <mecushka@gmail.com>
2023-03-11 11:09:35 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,39 @@
-import '../css/03-feedback.css';
-import '../css/common.css';
+// import '../css/03-feedback.css';
+// import '../css/common.css';
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form input'),
-    textarea: document.querySelector('.feedback-form textarea'),
-};
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
 
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.email.addEventListener('email', onEmail);
-// refs.textarea.addEventListener('textarea', onTextarea);
+form.addEventListener('submit', onFormSubmit);
+// email.addEventListener('email', onEmail);
+textarea.addEventListener('textarea', onTextarea);
 
-// function onTextarea(e) {
-//     const message = e.currentTarget.value;
 
-//     localStorage.setItem('feedback-msg', message)
-// }
 
-// const meaasge = "hallo";
-console.log("hallo");
+populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-msg');
+}
+
+function onTextarea(e) {
+    const message = e.currentTarget.value;
+
+    localStorage.setItem('feedback-msg', message)
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-msg')
+
+    if (savedMessage) {
+        console.log(savedMessage);
+    }
+
+}
+
+console.log("AAAA");
+console.log(localStorage);
